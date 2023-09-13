6ef4304ec9131f69daee9460a5a056c6e4cd0074
DESKTOP-0UDNDR3\VladI <gilad.dotann@gmail.com>
2023-09-13 17:46:13 +0300

    starting to build the files for the system

--- server/fileSystem.py
+++ server/fileSystem.py
@@ -1,6 +1,25 @@
+import copy
+
 # in this script we will be able to navigate the fake file system
 
-# files : [file / directory's name, the rest of the content]
-# each directory / file is a list with the second element separating
-# the first element is the file / directory's name
-def loadFiles(file)
+# the files / directories are as follows:
+# [name, flags : dict, the rest of the content]
+
+def loadFiles(soc, fileSystem):
+    # this variable will save the current folder we are in
+    curr_folder = copy.deepcopy(fileSystem)
+
+    soc.sendall("Please enter the system command you wish: ".encode())
+    command = soc.recv(1024).decode()
+
+    if command == "tree":
+        show_tree(curr_folder)
+
+def show_tree(curr_folder):
+    show_tree(curr_folder, '')
+def show_tree(curr_folder, spacer):
+    for i in range(len(curr_folder - 2)):
+        # printing out the files / directories names
+        print(spacer, curr_folder[2 + i][0])
+        if curr_folder[2 + i][1]["isDirectory"]:
+            show_tree(curr_folder[2 + i], spacer + " - ")
--- server/variables.py
+++ server/variables.py
@@ -1,3 +1,10 @@
+# login information
 Username = "Test"
 Password = "Test"
 NumOfLoginTries = 5
+
+# file system
+# flags : isDirectory, isAdministorPrivilage {only for files}, isAdminRestricted
+["root", {"isDirectory" : True, "isAdministorPrivilage" : False, "isAdminRestricted" : False},
+    ["users", {"isDirectory" : True, "isAdministorPrivilage" : False, "isAdminRestricted" : False},
+        []]]
