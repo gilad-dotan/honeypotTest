1694586afb9bdb0005f57fa9093fda3ac9064e6f
DESKTOP-0UDNDR3\VladI <gilad.dotann@gmail.com>
2023-09-13 16:16:07 +0300

    starting to build the file system manager

---
+++ server/fileSystem.py
@@ -0,0 +1,6 @@
+# in this script we will be able to navigate the fake file system
+
+# files : [file / directory's name, the rest of the content]
+# each directory / file is a list with the second element separating
+# the first element is the file / directory's name
+def loadFiles(file)
--- server/server.py
+++ server/server.py
@@ -3,31 +3,36 @@ import sys
 from login import *
 from variables import *
 
-HOST = ''
-PORT = 7000
+def init_server():
+    HOST = ''
+    PORT = 7000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('# Socket created')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('# Socket created')
 
-# Create socket on port
-try:
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print('# Bind failed. ')
-    sys.exit()
+    # Create socket on port
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as msg:
+        print('# Bind failed. ')
+        sys.exit()
 
-print('# Socket bind complete')
+    print('# Socket bind complete')
 
-# Start listening on socket
-s.listen(10)
-print('# Socket now listening')
+    # Start listening on socket
+    s.listen(10)
+    print('# Socket now listening')
 
-# Wait for client
-conn, addr = s.accept()
-print('# Connected to ' + addr[0] + ':' + str(addr[1]))
+    # Wait for client
+    conn, addr = s.accept()
+    print('# Connected to ' + addr[0] + ':' + str(addr[1]))
 
-login(conn, Username, Password, NumOfLoginTries)
+    # going to the login
+    login(conn, Username, Password, NumOfLoginTries)
 
+    # going to the "system"
 
+if __name__ == "__main__":
+    init_server()
 
 
